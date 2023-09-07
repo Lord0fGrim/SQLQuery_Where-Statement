a0ad9de80e754e3eb34a56bc628b7477662ad0cd
Lord0fGrim <115959756+Lord0fGrim@users.noreply.github.com>
2023-09-07 14:10:19 -0400

    Add files via upload

---
+++ SQLQuery_Where Statement.sql
@@ -0,0 +1,13 @@
+/*
+Where Statement 
+*, <>, <, >, And, Or, Like, Null, Not Null, In
+
+Different database
+[Name of the database].dbo.[table name]
+
+% = wildcards
+*/
+
+SELECT *
+FROM EmployeeDemographics
+WHERE FirstName IN ('Jim', 'Michael') 
